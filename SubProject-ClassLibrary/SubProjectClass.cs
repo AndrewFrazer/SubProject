9fbef2d13015a85a1909b82b9093caf33058dcc1
AndrewFrazerMP <Andrew.Frazer@malvernpanalytical.com>
2021-02-22 16:56:51 +0000

    initial commit

---
+++ SubProject-ClassLibrary/SubProjectClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedSource_ClassLibrary;
+
+namespace SubProject_ClassLibrary
+{
+    public static class SubProjectClass
+    {
+        public static int AddFromStrings(string a, string b) => SharedClass.Add(int.Parse(a), int.Parse(b));
+    }
+}
---
+++ SubProject-ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SubProject_ClassLibrary;
+
+namespace SubProject_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Count() == 2)
+            {
+                try
+                {
+                    Console.WriteLine(SubProjectClass.AddFromStrings(args[0], args[1]));
+                }
+                catch
+                {
+                    Console.WriteLine("Could not parse to ints");
+                }
+            }
+        }
+    }
+}
